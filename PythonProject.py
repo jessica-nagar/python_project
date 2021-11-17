39474dd9101a2746b8e4bba818deca5e3a383313
Mary Moor <mary@moor.cc>
2021-11-17 15:54:09 -0500

    Welcome-bot change

--- PythonProject.py
+++ PythonProject.py
@@ -22,7 +22,7 @@ intents = discord.Intents(members=True)
 client=discord.Client(intents=intents)
 
 @client.event
-async def on_ready():
+async def welcome_to():
     welcomechannel = await client.fetch_channel(909192549778989117)
     print('logged in as')
     print(client.user.name)
@@ -32,7 +32,7 @@ newUserMessage = "Welcome to our server!"
 
 @client.event
 async def on_member_join(member):
-    print("Recognised that a member called " + member.name + " joined")
+    print("Recognised that a member " + member.name + " joined")
     try: 
         await client.send_message(member, newUserMessage)
         print("Sent message to " + member.name)
@@ -42,13 +42,13 @@ async def on_member_join(member):
         title = "Welcome " + member.name + "!"
     )
         
-    role = discord.utils.get(member.server.roles, name="name-of-your-role")
-    await client.add_roles(member, role)
-    print("Added role '" + role.name + "' to " + member.name)
+ #   role = discord.utils.get(member.server.roles, name="name-of-your-role")
+ #   await client.add_roles(member, role)
+ #   print("Added role '" + role.name + "' to " + member.name)
 
 @client.event
-async def on_member_leave(member):
-    print("Recognised that a member called " + member.name + " left")
+async def goodbye_fairwell(member):
+    print("Recognised that a member " + member.name + " left")
     embed=discord.Embed(
         title="Goodbye "+member.name+"!",
     )
