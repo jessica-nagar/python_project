06b77e7b43968fa528b2db174f29c40464530041
Mary Moor <mary@moor.cc>
2021-11-23 21:34:37 -0500

    Version added to hello area

--- Python Project.py
+++ Python Project.py
@@ -15,5 +15,16 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
+    if message.content.startswith("$version"):
+        bot_test = client.get_channel(900825491961958430)
+
+        myEmbed = discord.Embed(title = "Current Verson", description = "The bot is in Version 2.0", color = 0x00ff00)
+        myEmbed.add_field(name = "Version Code: ", value = 'v2.0.0', inline = False)
+        myEmbed.add_field(name = "Date Released: ", value = "November 23, 2021", inline = False)
+        myEmbed.set_footer(text = "Version 1.0 done by Jess and Mary")
+        myEmbed.set_author(name = "Version Update by: Jess and Mary")
+
+        await bot_test.send(embed = myEmbed)
+
 client.run(os.getenv('TOKEN'))
 
